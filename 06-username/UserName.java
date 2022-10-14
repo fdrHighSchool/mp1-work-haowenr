eb37da27384cafaef9bc0d4b5a7bb5d89c4bba9a
AP Java Period 6 <apjavaperiod6@ac1mwk0lbjc9h.78b505.nycboe.org>
2022-10-14 12:57:45 -0400

    get user's role

--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -11,12 +11,14 @@ public class UserName {
     String firstName = s.nextLine();
     System.out.print("Enter your last name: ");
     String lastName = s.nextLine();
-
+    System.out.print("Are you a teacher or student? ");
+    String role = s.nextLine();
     
     System.out.println("Hello " + getInitial(firstName) + "." + getInitial(lastName) + ".");
 
     s.close();
   } 
+  
   public static String getInitial(String n) {
     return n.substring(0, 1);
   } 

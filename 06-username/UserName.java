a996b40eec5ee457e6c79cb8137b432ef3602637
AP Java Period 6 <apjavaperiod6@ac1mwk0lbjc9h.78b505.nycboe.org>
2022-10-14 12:56:19 -0400

    starter code

---
+++ 06-username/UserName.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class UserName {
+  public static void main(String[] args) {
+   
+    Scanner s = new Scanner(System.in);
+
+    
+    System.out.print("Enter your first name: ");
+    String firstName = s.nextLine();
+    System.out.print("Enter your last name: ");
+    String lastName = s.nextLine();
+
+    
+    System.out.println("Hello " + getInitial(firstName) + "." + getInitial(lastName) + ".");
+
+    s.close();
+  } 
+  public static String getInitial(String n) {
+    return n.substring(0, 1);
+  } 
+
+} 
